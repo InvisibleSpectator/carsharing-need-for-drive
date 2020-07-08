875dadfca229138f24c63ad07b102cc41224d5ae
InvisibleSpectator <vladislav5678@yandex.ru>
2020-07-08 22:02:19 +0400

    Updates

---
+++ precache-manifest.09782d096d5d788bac17687914ade26e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b337f2ed312e857f8b9444975f8218e1",
+    "url": "/carsharing-need-for-drive/index.html"
+  },
+  {
+    "revision": "fd29034ff260c650976a",
+    "url": "/carsharing-need-for-drive/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f8cbc77058045859d0d5",
+    "url": "/carsharing-need-for-drive/static/js/2.bcc756b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/carsharing-need-for-drive/static/js/2.bcc756b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd29034ff260c650976a",
+    "url": "/carsharing-need-for-drive/static/js/main.7e519d6f.chunk.js"
+  },
+  {
+    "revision": "88d8c6249f47d66274a3",
+    "url": "/carsharing-need-for-drive/static/js/runtime-main.f99e8ff4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/carsharing-need-for-drive/static/media/logo.5d5d9eef.svg"
+  }
+]);
