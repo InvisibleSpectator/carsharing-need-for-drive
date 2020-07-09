ce9fd6043e45d6057746160f911e45c722527bdb
InvisibleSpectator <vladislav5678@yandex.ru>
2020-07-10 00:56:32 +0400

    preparation

---
+++ src/components/core/Button/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Button.scss";
+
+const Button = (props) => {
+  return (
+    <button className={`Button ${props.className}`}>
+      <span className={props.className.includes("Button_loading") ? "Button-Text Button-Text_loading" : "Button-Text"}>OK</span>
+      <div className={props.className.includes("Button_loading") ? "Button-Spinner" : "Button-Spinner_disabled"}></div>
+    </button>
+  );
+};
+
+Button.defaultProps = { className: "Button_default" };
+
+export default Button;
