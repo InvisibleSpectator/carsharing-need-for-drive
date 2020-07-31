0a76646f2af12adf23e7cbbc6a9a31cdc57fc335
InvisibleSpectator <vladislav5678@yandex.ru>
2020-08-01 03:30:57 +0400

    fix for gh pages

--- src/components/Extra/Extra.js
+++ src/components/Extra/Extra.js
@@ -30,7 +30,7 @@ class Extra extends Component {
 
   componentDidMount = async () => {
     const rateResponse = await fetch(
-      "http://api-factory.simbirsoft1.com/api/db/rate",
+      "https://cors-anywhere.herokuapp.com/http://api-factory.simbirsoft1.com/api/db/rate",
       {
         method: "GET",
         headers: { "X-Api-Factory-Application-Id": "5e25c641099b810b946c5d5b" },
