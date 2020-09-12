7e1e378726e5da531011c12b67da40dd2207e621
InvisibleSpectator <vladislav5678@yandex.ru>
2020-09-12 19:38:50 +0400

    V-08 create component

---
+++ src/adminStuff/OrderList/OrderList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class OrderList extends React.Component {
+  render = () => {
+    return (
+      <div className="OrderList">
+        <h2 className="AdminPage-Title">Список заказов</h2>
+        <div className="AdminStyledBlock">
+          <div className="AdminStyledBlock-Content"></div>
+        </div>
+      </div>
+    );
+  };
+}
+
+export default OrderList;
